53904ed57141a71b32dbabf2a7d8817da232519d
AlexandrSerebryakov <common190516@mail.ru>
2016-10-22 20:00:13 +0600

    Change comments.

--- src/main/java/com/epam/alex/entity/Lexeme.java
+++ src/main/java/com/epam/alex/entity/Lexeme.java
@@ -1,7 +1,7 @@
 package com.epam.alex.entity;
 
 /**
- * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
+ * Created by Alexandr Serebryakov on 22.10.2016.
  */
 public class Lexeme extends Listing {
 }
--- src/main/java/com/epam/alex/main/Runner.java
+++ src/main/java/com/epam/alex/main/Runner.java
@@ -1,7 +1,7 @@
 package com.epam.alex.main;
 
 /**
- * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
+ * Created by Alexandr Serebryakov on 22.10.2016.
  */
 public class Runner {
     public static void main(String[] args) {
