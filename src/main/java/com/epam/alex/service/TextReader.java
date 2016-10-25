033a83489468ce9b2ec4ac0f3ba1df14544af592
AlexandrSerebryakov <common190516@mail.ru>
2016-10-26 02:51:22 +0600

    Edit class "TextReader".

--- src/main/java/com/epam/alex/service/TextReader.java
+++ src/main/java/com/epam/alex/service/TextReader.java
@@ -1,7 +1,20 @@
 package com.epam.alex.service;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by Alexandr Serebryakov on 22.10.2016.
  */
 public class TextReader {
+    public static String readText() throws FileNotFoundException {
+        String text = "";
+        Scanner scanner = new Scanner(new File("text.txt"));
+        while (scanner.hasNextLine()) {
+            text += scanner.nextLine() + '\n';
+
+        }
+        return text;
+    }
 }
